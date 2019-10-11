87807d938c291909b58d46c55d7d5b387206abfb
ksage <ksage@zinnaworks.com>
2019-10-11 14:45:57 +0900

    User Operations func

--- iOS/Calculator/Calculator/CalculatorBrain.swift
+++ iOS/Calculator/Calculator/CalculatorBrain.swift
@@ -9,22 +9,35 @@
 import Foundation
 
 class CalculatorBrain {
-    
+        
     private var accumulator = 0.0
     func setOperand(operand: Double) {
         accumulator = operand
     }
     
-    var operations: Dictionary<String, Double> = [
-        "π" : Double.pi,
-        "e" : M_E,
-        "⎷" : sqrt,
-        "cos" : cos`
+    var operations: Dictionary<String, Operation> = [
+        "π" : Operation.Constant(Double.pi),
+        "e" : Operation.Constant(M_E),
+        "⎷" : Operation.UnaryOperation(sqrt),
+        "cos" : Operation.UnaryOperation(cos)
     ]
     
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation
+        case Equals
+    }
+    
     func performOperation(symbol: String) {
-        if let contant = operations[symbol] {
-            accumulator = contant
+        if let operation = operations[symbol] {
+            switch operation {
+                case .Constant(let value): accumulator = value
+                case .UnaryOperation(let function): accumulator = function(accumulator)
+                case .BinaryOperation: break
+                case .Equals: break
+                default: break
+            }
         }
     }
     
